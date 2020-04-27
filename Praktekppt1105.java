9e6456c81b87e851c836324005e4496c08c24a9b
wilsonoey60 <61810752+wilsonoey60@users.noreply.github.com>
2020-04-27 16:13:12 +0700

    Add files via upload

---
+++ Praktekppt1105.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Praktekppt1105 { 
+	public static void main(String[] arg) {
+		int aa = 5;
+		int bb = 6;
+		double cc = 0.5;
+		System.out.println("aa + bb = " +(aa+bb));
+		System.out.println("aa * bb * cc = " +(aa*bb*cc));
+		System.out.println("bb / cc = " +(bb/cc));
+	}
+} 
---
+++ Praktekppt1205.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class MyClass {
+  public static void main(String[ ] args) {
+    Scanner Saudara = new Scanner(System.in);
+    System.out.print("Saudara :");
+    System.out.println(Saudara.nextLine());
+    Scanner Tinggal_di = new Scanner(System.in);
+    System.out.print("Tinggal di :");
+    System.out.println(Tinggal_di.nextLine());
+    Scanner Umur = new Scanner(System.in);
+    System.out.print("Umur :");
+    System.out.println(Umur.nextLine());        
+  }
+}
